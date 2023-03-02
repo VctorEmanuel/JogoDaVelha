51debc37433fa48ae106bbf54d86856edcc0b0a5
victo <victo@DESKTOP-BS6692E>
2023-03-02 20:32:33 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt0_0 = new System.Windows.Forms.Button();
+            this.bt0_1 = new System.Windows.Forms.Button();
+            this.bt0_2 = new System.Windows.Forms.Button();
+            this.bt1_2 = new System.Windows.Forms.Button();
+            this.bt1_1 = new System.Windows.Forms.Button();
+            this.bt1_0 = new System.Windows.Forms.Button();
+            this.bt2_2 = new System.Windows.Forms.Button();
+            this.bt2_1 = new System.Windows.Forms.Button();
+            this.bt2_0 = new System.Windows.Forms.Button();
+            this.gbEstrutura = new System.Windows.Forms.GroupBox();
+            this.lbVez = new System.Windows.Forms.Label();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.btJogar = new System.Windows.Forms.Button();
+            this.gbRegistro = new System.Windows.Forms.GroupBox();
+            this.txtBoxRegistro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btJogarNovamente = new System.Windows.Forms.Button();
+            this.gbEstrutura.SuspendLayout();
+            this.gbRegistro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt0_0
+            // 
+            this.bt0_0.BackColor = System.Drawing.Color.Snow;
+            this.bt0_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt0_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt0_0.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bt0_0.FlatAppearance.BorderSize = 0;
+            this.bt0_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt0_0.Location = new System.Drawing.Point(64, 58);
+            this.bt0_0.Name = "bt0_0";
+            this.bt0_0.Size = new System.Drawing.Size(74, 74);
+            this.bt0_0.TabIndex = 0;
+            this.bt0_0.UseVisualStyleBackColor = false;
+            this.bt0_0.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt0_1
+            // 
+            this.bt0_1.BackColor = System.Drawing.Color.Snow;
+            this.bt0_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt0_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt0_1.FlatAppearance.BorderSize = 0;
+            this.bt0_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt0_1.Location = new System.Drawing.Point(144, 58);
+            this.bt0_1.Name = "bt0_1";
+            this.bt0_1.Size = new System.Drawing.Size(74, 74);
+            this.bt0_1.TabIndex = 1;
+            this.bt0_1.UseVisualStyleBackColor = false;
+            this.bt0_1.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt0_2
+            // 
+            this.bt0_2.BackColor = System.Drawing.Color.Snow;
+            this.bt0_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt0_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt0_2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.bt0_2.FlatAppearance.BorderSize = 0;
+            this.bt0_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt0_2.Location = new System.Drawing.Point(223, 58);
+            this.bt0_2.Name = "bt0_2";
+            this.bt0_2.Size = new System.Drawing.Size(74, 74);
+            this.bt0_2.TabIndex = 2;
+            this.bt0_2.UseVisualStyleBackColor = false;
+            this.bt0_2.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt1_2
+            // 
+            this.bt1_2.BackColor = System.Drawing.Color.Snow;
+            this.bt1_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt1_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt1_2.FlatAppearance.BorderSize = 0;
+            this.bt1_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt1_2.Location = new System.Drawing.Point(223, 138);
+            this.bt1_2.Name = "bt1_2";
+            this.bt1_2.Size = new System.Drawing.Size(74, 74);
+            this.bt1_2.TabIndex = 5;
+            this.bt1_2.UseVisualStyleBackColor = false;
+            this.bt1_2.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt1_1
+            // 
+            this.bt1_1.BackColor = System.Drawing.Color.Snow;
+            this.bt1_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt1_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt1_1.FlatAppearance.BorderSize = 0;
+            this.bt1_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt1_1.Location = new System.Drawing.Point(143, 138);
+            this.bt1_1.Name = "bt1_1";
+            this.bt1_1.Size = new System.Drawing.Size(74, 74);
+            this.bt1_1.TabIndex = 4;
+            this.bt1_1.UseVisualStyleBackColor = false;
+            this.bt1_1.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt1_0
+            // 
+            this.bt1_0.BackColor = System.Drawing.Color.Snow;
+            this.bt1_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt1_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt1_0.FlatAppearance.BorderSize = 0;
+            this.bt1_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt1_0.Location = new System.Drawing.Point(64, 138);
+            this.bt1_0.Name = "bt1_0";
+            this.bt1_0.Size = new System.Drawing.Size(74, 74);
+            this.bt1_0.TabIndex = 3;
+            this.bt1_0.UseVisualStyleBackColor = false;
+            this.bt1_0.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt2_2
+            // 
+            this.bt2_2.BackColor = System.Drawing.Color.Snow;
+            this.bt2_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt2_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt2_2.FlatAppearance.BorderSize = 0;
+            this.bt2_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt2_2.Location = new System.Drawing.Point(224, 218);
+            this.bt2_2.Name = "bt2_2";
+            this.bt2_2.Size = new System.Drawing.Size(74, 74);
+            this.bt2_2.TabIndex = 8;
+            this.bt2_2.UseVisualStyleBackColor = false;
+            this.bt2_2.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt2_1
+            // 
+            this.bt2_1.BackColor = System.Drawing.Color.Snow;
+            this.bt2_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt2_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt2_1.FlatAppearance.BorderSize = 0;
+            this.bt2_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt2_1.Location = new System.Drawing.Point(144, 218);
+            this.bt2_1.Name = "bt2_1";
+            this.bt2_1.Size = new System.Drawing.Size(74, 74);
+            this.bt2_1.TabIndex = 7;
+            this.bt2_1.UseVisualStyleBackColor = false;
+            this.bt2_1.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // bt2_0
+            // 
+            this.bt2_0.BackColor = System.Drawing.Color.Snow;
+            this.bt2_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt2_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt2_0.FlatAppearance.BorderSize = 0;
+            this.bt2_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt2_0.Location = new System.Drawing.Point(64, 218);
+            this.bt2_0.Name = "bt2_0";
+            this.bt2_0.Size = new System.Drawing.Size(74, 74);
+            this.bt2_0.TabIndex = 6;
+            this.bt2_0.UseVisualStyleBackColor = false;
+            this.bt2_0.Click += new System.EventHandler(this.botaoClicado);
+            // 
+            // gbEstrutura
+            // 
+            this.gbEstrutura.AccessibleDescription = "Jogo da velha";
+            this.gbEstrutura.BackColor = System.Drawing.Color.IndianRed;
+            this.gbEstrutura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gbEstrutura.Controls.Add(this.lbVez);
+            this.gbEstrutura.Controls.Add(this.bt0_2);
+            this.gbEstrutura.Controls.Add(this.bt2_2);
+            this.gbEstrutura.Controls.Add(this.bt0_1);
+            this.gbEstrutura.Controls.Add(this.bt2_1);
+            this.gbEstrutura.Controls.Add(this.bt0_0);
+            this.gbEstrutura.Controls.Add(this.bt2_0);
+            this.gbEstrutura.Controls.Add(this.bt1_1);
+            this.gbEstrutura.Controls.Add(this.bt1_2);
+            this.gbEstrutura.Controls.Add(this.bt1_0);
+            this.gbEstrutura.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gbEstrutura.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.gbEstrutura.Location = new System.Drawing.Point(213, 101);
+            this.gbEstrutura.Name = "gbEstrutura";
+            this.gbEstrutura.Size = new System.Drawing.Size(362, 317);
+            this.gbEstrutura.TabIndex = 9;
+            this.gbEstrutura.TabStop = false;
+            this.gbEstrutura.Visible = false;
+            // 
+            // lbVez
+            // 
+            this.lbVez.AutoSize = true;
+            this.lbVez.BackColor = System.Drawing.Color.IndianRed;
+            this.lbVez.Font = new System.Drawing.Font("Ink Free", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVez.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbVez.Location = new System.Drawing.Point(113, 16);
+            this.lbVez.Name = "lbVez";
+            this.lbVez.Size = new System.Drawing.Size(135, 39);
+            this.lbVez.TabIndex = 10;
+            this.lbVez.Text = "Vez de X";
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Font = new System.Drawing.Font("Ink Free", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbTitulo.Location = new System.Drawing.Point(263, 36);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(252, 46);
+            this.lbTitulo.TabIndex = 10;
+            this.lbTitulo.Text = "Jogo da Velha";
+            // 
+            // btJogar
+            // 
+            this.btJogar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btJogar.Font = new System.Drawing.Font("Ink Free", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btJogar.Location = new System.Drawing.Point(317, 201);
+            this.btJogar.Name = "btJogar";
+            this.btJogar.Size = new System.Drawing.Size(146, 49);
+            this.btJogar.TabIndex = 1;
+            this.btJogar.Text = "JOGAR";
+            this.btJogar.UseVisualStyleBackColor = true;
+            this.btJogar.Click += new System.EventHandler(this.botaoJogar);
+            // 
+            // gbRegistro
+            // 
+            this.gbRegistro.AccessibleDescription = "Jogo da velha";
+            this.gbRegistro.BackColor = System.Drawing.Color.IndianRed;
+            this.gbRegistro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gbRegistro.Controls.Add(this.txtBoxRegistro);
+            this.gbRegistro.Controls.Add(this.label2);
+            this.gbRegistro.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gbRegistro.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.gbRegistro.Location = new System.Drawing.Point(616, 101);
+            this.gbRegistro.Name = "gbRegistro";
+            this.gbRegistro.Size = new System.Drawing.Size(152, 317);
+            this.gbRegistro.TabIndex = 11;
+            this.gbRegistro.TabStop = false;
+            this.gbRegistro.Visible = false;
+            // 
+            // txtBoxRegistro
+            // 
+            this.txtBoxRegistro.Enabled = false;
+            this.txtBoxRegistro.Location = new System.Drawing.Point(6, 69);
+            this.txtBoxRegistro.Multiline = true;
+            this.txtBoxRegistro.Name = "txtBoxRegistro";
+            this.txtBoxRegistro.Size = new System.Drawing.Size(141, 223);
+            this.txtBoxRegistro.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.IndianRed;
+            this.label2.Font = new System.Drawing.Font("Ink Free", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 39);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Registro";
+            // 
+            // btJogarNovamente
+            // 
+            this.btJogarNovamente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btJogarNovamente.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btJogarNovamente.Location = new System.Drawing.Point(12, 107);
+            this.btJogarNovamente.Name = "btJogarNovamente";
+            this.btJogarNovamente.Size = new System.Drawing.Size(185, 49);
+            this.btJogarNovamente.TabIndex = 12;
+            this.btJogarNovamente.Text = "JOGAR NOVAMENTE";
+            this.btJogarNovamente.UseVisualStyleBackColor = true;
+            this.btJogarNovamente.Visible = false;
+            this.btJogarNovamente.Click += new System.EventHandler(this.jogarNovamente);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btJogarNovamente);
+            this.Controls.Add(this.btJogar);
+            this.Controls.Add(this.gbRegistro);
+            this.Controls.Add(this.lbTitulo);
+            this.Controls.Add(this.gbEstrutura);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.gbEstrutura.ResumeLayout(false);
+            this.gbEstrutura.PerformLayout();
+            this.gbRegistro.ResumeLayout(false);
+            this.gbRegistro.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt0_0;
+        private System.Windows.Forms.Button bt0_1;
+        private System.Windows.Forms.Button bt0_2;
+        private System.Windows.Forms.Button bt1_2;
+        private System.Windows.Forms.Button bt1_1;
+        private System.Windows.Forms.Button bt1_0;
+        private System.Windows.Forms.Button bt2_2;
+        private System.Windows.Forms.Button bt2_1;
+        private System.Windows.Forms.Button bt2_0;
+        private System.Windows.Forms.Label lbVez;
+        private System.Windows.Forms.GroupBox gbEstrutura;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Button btJogar;
+        private System.Windows.Forms.GroupBox gbRegistro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxRegistro;
+        private System.Windows.Forms.Button btJogarNovamente;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha {
+    public partial class Form1 : Form {
+
+        public bool venceuOuNao = false;
+
+        public int rodada = 0;
+
+        public char vez = 'X';
+
+        public string posicaoJogada = "";
+        public string registro = "";
+
+        public int[,] tabuleiroX = new int[3,3];
+        public int[,] tabuleiroO = new int[3,3];
+
+        public Form1() {
+
+            InitializeComponent();
+
+        }
+
+        private void botaoJogar(object sender, EventArgs e) {
+
+            btJogar.Visible = false;
+            gbEstrutura.Visible = true;
+            gbRegistro.Visible = true;
+
+        }
+
+        private void botaoClicado(object sender, EventArgs e) {
+
+            Button botao = (Button)sender;
+            this.rodada++;
+
+            this.posicaoJogada = botao.Name.Substring(2, 3).Replace("_", ", ");
+            txtBoxRegistro.Text += "\r\n" + this.vez + " - Jogou na posição " + this.posicaoJogada;
+
+            atribuirValoresTabuleiro(posicaoJogada.ToCharArray(), this.vez);
+            atribuirImagem(botao);
+
+            this.venceuOuNao = verificarTabuleiro(this.vez == 'X' ? tabuleiroX : tabuleiroO);
+
+            if (this.venceuOuNao) {
+                lbVez.Text = this.vez + " VENCEU!!!!!";
+                txtBoxRegistro.Text += "\r\n" + this.vez + " - venceu";
+                btJogarNovamente.Visible = true;
+                bloquearTabuleiro();
+            } else {
+                if (this.rodada == 9) {
+                    lbVez.Text = "EMPATE";
+                    txtBoxRegistro.Text += "\r\n empatou";
+                    btJogarNovamente.Visible = true;
+                } else {
+                    mudarVez();
+                    lbVez.Text = "Vez de " + this.vez;
+                }
+            }
+        }
+
+        private void atribuirImagem(Button botao) {
+
+            if (this.vez.Equals('X')) {
+                botao.BackgroundImage = Properties.Resources.x;
+            } else {
+                botao.BackgroundImage = Properties.Resources.o;
+            }
+
+            botao.Enabled = false;
+
+        }
+
+        private void mudarVez() {
+
+            this.vez = this.vez == 'O' ? 'X' : 'O';
+
+        }
+
+        private void atribuirValoresTabuleiro(char[] posicao, char vez) {
+
+            int x = (int)Char.GetNumericValue(posicao[0]);
+            int y = (int)Char.GetNumericValue(posicao[3]);
+
+            if (vez == 'X') {
+                this.tabuleiroX[x, y] = 1;
+            } else {
+                this.tabuleiroO[x, y] = 1;
+            }
+            
+        }
+
+        private bool verificarTabuleiro(int[,] tabuleiro) {
+            
+            //Verificar diagonais
+            if (tabuleiro[0, 0] + tabuleiro[1, 1] + tabuleiro[2, 2] == 3) {
+                return true;
+            } else if (tabuleiro[2, 0] + tabuleiro[1, 1] + tabuleiro[0, 2] == 3) {
+                return true;
+            }
+
+            //Verificar Linhas
+            for (int x = 0; x < 2; x++) {
+                if (tabuleiro[x, 0] + tabuleiro[x, 1] + tabuleiro[x, 2] == 3) {
+                    return true;
+                }
+            }
+
+            //Verificar Linhas
+            for (int y = 0; y < 2; y++) {
+                if (tabuleiro[0, y] + tabuleiro[1, y] + tabuleiro[2, y] == 3) {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+        private void bloquearTabuleiro() {
+            bt0_0.Enabled = false;
+            bt0_1.Enabled = false;
+            bt0_2.Enabled = false;
+            bt1_0.Enabled = false;
+            bt1_1.Enabled = false;
+            bt1_2.Enabled = false;
+            bt2_0.Enabled = false;
+            bt2_1.Enabled = false;
+            bt2_2.Enabled = false;
+        }
+
+        private void desbloquearTabuleiro() {
+            bt0_0.Enabled = true;
+            bt0_0.BackgroundImage = null;
+            bt0_1.Enabled = true;
+            bt0_1.BackgroundImage = null;
+            bt0_2.Enabled = true;
+            bt0_2.BackgroundImage = null;
+            bt1_0.Enabled = true;
+            bt1_0.BackgroundImage = null;
+            bt1_1.Enabled = true;
+            bt1_1.BackgroundImage = null;
+            bt1_2.Enabled = true;
+            bt1_2.BackgroundImage = null;
+            bt2_0.Enabled = true;
+            bt2_0.BackgroundImage = null;
+            bt2_1.Enabled = true;
+            bt2_1.BackgroundImage = null;
+            bt2_2.Enabled = true;
+            bt2_2.BackgroundImage = null;
+        }
+
+        private void jogarNovamente(object sender, EventArgs e) {
+
+            btJogarNovamente.Visible = false;
+            this.rodada = 0;
+            this.tabuleiroO = new int[3, 3];
+            this.tabuleiroX = new int[3, 3];
+            this.vez = 'X';
+            lbVez.Text = "Vez de X";
+            txtBoxRegistro.Text = "";
+            desbloquearTabuleiro();
+
+        }
+    }
+}
